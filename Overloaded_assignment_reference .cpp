58975a6c012c47f5c660840600b60cafaeb8a834
prateekrawal <prateek_2k15b1150@dtu.ac.in>
2018-07-04 21:43:01 +0530

    overload assignment referce

---
+++ Overloaded_assignment_reference .cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class alpha
+{
+private:
+	int data;
+public:
+	alpha()
+	{}
+	alpha(int d)
+	{
+		data =d;
+	}
+	void display()
+	{
+		cout<<"Data is "<<data <<endl;
+	}
+	alpha& operator = (alpha& a)
+	{
+		data=a.data;
+		cout<<"Assignment operator invoked \n";
+		return *this;
+	}
+};
+int main()
+{
+	alpha a1(36);
+	alpha a2,a3;
+	a3=a2=a1;
+	cout<<"A2";a2.display();
+	cout<<"A3 ";a3.display();
+	return 0;
+}
