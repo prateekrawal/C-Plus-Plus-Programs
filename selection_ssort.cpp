09142fc68bc8251f3f6c045b36ca09575a1d59a6
prateekrawal <prateek_2k15b1150@dtu.ac.in>
2018-07-09 21:29:01 +0530

    Selection Sort

--- selection_ssort.cpp
+++ selection_ssort.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+void selectionsort(int*a,int n);
+void swap(int*a,int*b);
 int main()
 {
 	int n;
@@ -7,5 +9,29 @@ int main()
 	int a[n];
 	for(int i=0;i<n;i++)
 		cin>>a[i];
+	selectionsort(a,n);
+	for(int i=0;i<n;i++)
+		cout<<a[i]<<"  ";
 	return 0;
+}
+
+void selectionsort(int*a,int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		int min=i;
+		for(int j=i;j<n;j++)
+		{
+			if(a[j]<a[min])
+				min=j;
+		}
+		swap(a[i],a[min]);
+	}
+}
+void swap(int*a ,int *b)
+{
+	int* temp;
+	temp=a;
+	a=b;
+	b=temp;
 }
