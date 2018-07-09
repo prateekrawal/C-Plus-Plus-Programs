4e5a064484267190c8d04a69b8f40f254e8f1b8a
prateekrawal <prateek_2k15b1150@dtu.ac.in>
2018-07-09 20:28:42 +0530

    Mirror Inverted

--- MirrorInverted2.cpp
+++ MirrorInverted2.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
-int* checkMirrorInvert(int*a ,int n);
+void checkMirrorInvert(int*a ,int n);
 int main()
 {
 	int n;
 	cin>>n;
-	int a[n,b[n];
+	int a[n],b[n];
 	for(int i=0;i<n;i++)
 		cin>>a[i];
 	for(int k=0;k<n;k++)
 		cin>>b[k];
 	int s[n];
-	s=checkMirrorInvert(a,n);
+	checkMirrorInvert(a,n);
 	int flag=0;
 	for(int j=0;j<n;j++)
 	{
-		if(s[j]!= b[j])
+		if(a[j]!= b[j])
 		{
 			flag=1;
 			break;
@@ -27,11 +27,12 @@ int main()
 		cout<<"Not inverse\n";
 	return 0;
 }
-int* checkMirrorInvert(int*a ,int n)
+void checkMirrorInvert(int*a ,int n)
 {
 	int s[n];
 	int flag=0;
 	for(int i=0;i<n;i++)
 			s[a[i]-1]=i+1;
-	return s;
+    for(int i=0;i<n;i++)
+        a[i]=s[i];
 }
