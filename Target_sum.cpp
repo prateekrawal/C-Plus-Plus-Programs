e336b2ed0126571b39981d154db8d79bb42ad350
prateekrawal <prateek_2k15b1150@dtu.ac.in>
2018-07-13 19:22:19 +0530

    TArget Sum

--- Target_sum.cpp
+++ Target_sum.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
 using namespace std;
 #include <vector>
+#include <algorithm>
 int main()
 {
 	int n,a;
+	int target;
 	cin>>n;
-	vector<int> v;
+    int v[n];
 	for(int i=0;i<n;i++)
-	{	
+	{
 		cin>>a;
-		v.push_back(a);
+		v[i]=(a);
 	}
+	cin>>target;
+	sort(v,v+n);
+	int i=0,j=n-1;
+	while(i<j)
+	{
+		int a=v[i];
+		int rem=target-a;
+		if(rem==v[j])
+		{
+			cout<<"Elements are "<<v[i]<<"  "<<v[j]<<endl;
+			i++;
+		}
+		else if(rem>v[j])
+		{
+			i++;
+		}
+		else
+		{
+			j--;
+		}
+
+	}
+
+	cout<<"Enter the target Sum\n";
+	cin>>target;
 	return 0;
 }
