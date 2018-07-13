c15939da596587b4920dcfae87de42ca5ebe66b5
prateekrawal <prateek_2k15b1150@dtu.ac.in>
2018-07-13 18:17:53 +0530

    MAp

---
+++ Target_sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+int main()
+{
+	int n,a;
+	cin>>n;
+	vector<int> v;
+	for(int i=0;i<n;i++)
+	{	
+		cin>>a;
+		v.push_back(a);
+	}
+	return 0;
+}
