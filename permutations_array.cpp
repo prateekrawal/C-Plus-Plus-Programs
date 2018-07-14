fe3cd0211e0c298a5d4174fef9097d4d87ab461c
prateekrawal <prateek_2k15b1150@dtu.ac.in>
2018-07-14 13:55:07 +0530

    Permutation array

--- permutations_array.cpp
+++ permutations_array.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 using namespace std;
+int n;
+void pr(int* a,int n)
+{
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+}
+void swap(int*a,int*b)
+{
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
+void permute(int*a,int l,int r)
+{
+	int i;
+	if(l==r)
+		pr(a,n);
+	else
+	{
+		for(i=l;i<=r;i++)
+		{
+			swap(a+l,a+i);
+			permute(a,l+1,r);
+			swap(a+l,a+i);
+		}
+	}
+}
 int main()
 {
-	int n;
 	cin>>n;
 	int a[n];
 	for(int i=0;i<n;i++)
 		cin>>a[i];
+	permute(a,0,n-1);
 	return 0;
 }
